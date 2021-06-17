017344804c7eb4469540dcdade8352b9f9f4d4b1
Adrian Łukasik <adrianlukasik994@gmail.com>
2021-06-17 23:50:46 +0200

    Added game board

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    table_size = 8
+    return render_template('home.html', table_size=table_size)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
