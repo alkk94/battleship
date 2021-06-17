adf9bc53e6f0dfca266d2a880d19945df7e60922
Adrian Łukasik <adrianlukasik994@gmail.com>
2021-06-18 00:37:24 +0200

    Set ids for table elements

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     table_size = 8
-    return render_template('home.html', table_size=table_size)
+    return render_template('home.html', table_size=table_size, str=str)
 
 
 if __name__ == '__main__':
